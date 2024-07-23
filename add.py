c3fd8f7d07be9779b358a4fcc23c2d8b7024553d
Vidyaaa123 <156874755+Vidyaaa123@users.noreply.github.com>
2024-07-23 12:21:28 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Simple addition program
+
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform addition
+sum = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is", sum)
+
